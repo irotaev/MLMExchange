329c4dc9c90679d7d88c91e31f0c1ff15864c8eb
SUPER_MEN4iG <supermen4ig988@gmail.com>
2014-03-16 22:10:16 +0200

    home commit

--- MLMExchange/Controllers/HomeController.cs
+++ MLMExchange/Controllers/HomeController.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using MLMExchange.Lib;
 
 namespace MLMExchange.Controllers
 {
-  public class HomeController : BaseController
+  public class HomeController : Controller
   {
-    [Auth]
     public ActionResult Index()
     {
       return View();
