bc5af48bcdd0f1914576926932df50fe4c6efd91
SUPER_MEN4iG <supermen4ig988@gmail.com>
2014-03-19 13:27:59 +0200

    add commit

---
+++ MLMExchange/Controllers/ContactsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MLMExchange.Controllers
+{
+    public class ContactsController : Controller
+    {
+        //
+        // GET: /Contacts/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
