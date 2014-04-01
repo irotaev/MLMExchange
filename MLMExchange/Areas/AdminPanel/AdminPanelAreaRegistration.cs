e0aa781b1b2222a0a6dd3ff01876164ebcacd1e4
irotaev <irotaev@gmail.com>
2014-04-01 11:04:02 +0400

    work | Ext.Net example

---
+++ MLMExchange/Areas/AdminPanel/AdminPanelAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MLMExchange.Areas.AdminPanel
+{
+  public class AdminPanelAreaRegistration : AreaRegistration
+  {
+    public override string AreaName
+    {
+      get
+      {
+        return "AdminPanel";
+      }
+    }
+
+    public override void RegisterArea(AreaRegistrationContext context)
+    {
+      context.MapRoute(
+          "AdminPanel",
+          "AdminPanel/{controller}/{action}/{id}",
+          new { id = UrlParameter.Optional }
+      );
+    }
+  }
+}
---
+++ MLMExchange/Areas/AdminPanel/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using MLMExchange.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MLMExchange.Areas.AdminPanel.Controllers
+{
+  public class UserController : BaseController
+  {
+    public ActionResult Info()
+    {
+      return View();
+    }
+  }
+}
