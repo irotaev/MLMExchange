4a029cfbe8f5f5a07e796eea225445efbba26fba
irotaev <irotaev@gmail.com>
2014-04-14 22:14:51 +0400

    add ext.net 2.5

--- MLMExchange/WebLogic/Authorization.cs
+++ MLMExchange/WebLogic/Authorization.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Providers.Entities;
 
 namespace MLMExchange.Lib
 {
--- MLMExchange/WebLogic/CurrentSession.cs
+++ MLMExchange/WebLogic/CurrentSession.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Providers.Entities;
 using Logic;
 using System.Web.SessionState;
 
