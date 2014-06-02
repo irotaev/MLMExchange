28f354efc950aa80eb12a6b94dd938dfca565e8e
irotaev <irotaev@gmail.com>
2014-06-03 00:46:04 +0400

    Add Admin main menu javascript

---
+++ MLMExchange/Scripts/Lib/Menu/AdminMainMenu.js
@@ -0,0 +1,30 @@
+﻿/// <reference path="../../typings/jquery/jquery.d.ts" />
+define(["require", "exports", "jquery"], function(require, exports) {
+    /// <amd-dependency path="jquery" />
+    $ = require("jquery");
+
+    /**
+    * Главное меню админ-панели
+    */
+    var AdminMainMenu = (function () {
+        function AdminMainMenu($menu) {
+            this._$Menu = $menu;
+        }
+        /**
+        * Выделить пункт меню.
+        * Выделяет пункт меню, в зависимости от текущего url
+        */
+        AdminMainMenu.prototype.SelectMenuItem = function (num) {
+            var url = window.location.href;
+
+            this._$Menu.find("a").each(function () {
+                if (url == (this.href)) {
+                    $(this).find("nav").addClass("pb-mm__menu-item-wrapper-active");
+                }
+            });
+        };
+        return AdminMainMenu;
+    })();
+    exports.AdminMainMenu = AdminMainMenu;
+});
+//# sourceMappingURL=AdminMainMenu.js.map
---
+++ MLMExchange/Scripts/Lib/Menu/AdminMainMenu.ts
@@ -0,0 +1,37 @@
+﻿/// <reference path="../../typings/jquery/jquery.d.ts" />
+
+/// <amd-dependency path="jquery" /> 
+
+$ = require("jquery");
+
+import FormModule = require("../Form");
+
+/**
+ * Главное меню админ-панели
+ */
+export class AdminMainMenu
+{
+  constructor($menu: JQuery)
+  {
+    this._$Menu = $menu;
+  }
+
+  private _$Menu: JQuery;
+
+  /**
+   * Выделить пункт меню.
+   * Выделяет пункт меню, в зависимости от текущего url
+   */
+  public SelectMenuItem(num: (num: number, name: string) => boolean): void
+  {
+    var url = window.location.href;
+
+    this._$Menu.find("a").each(function ()
+    {
+      if (url == (this.href))
+      {
+        $(this).find("nav").addClass("pb-mm__menu-item-wrapper-active");
+      }
+    });
+  }
+}
